fc1caae97b513021b370c859d3b563250bc38bcc
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-06-19 23:35:25 +0300

    added test

--- jest/test/unit/sortByName.test.js
+++ jest/test/unit/sortByName.test.js
@@ -1,17 +1,43 @@
 const sorting = require("../../app");
 
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+  test("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+    const result = sorting.sortByName(input);
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+    expect(result).toEqual(expected);
   });
-});
+    it("Book name sorted", () => {
+     const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+     ];
+     const result = sorting.sortByName(input);
+     const expected = [
+      "Властелин Колец",
+       "Гарри Поттер",
+     ];
+     expect(result).toEqual(expected);
+    });
+    it("Book same names", () => {
+      const input = [
+        "Властелин Колец",
+        "Властелин Колец",
+      ];
+      const result = sorting.sortByName(input);
+      const expected = [
+        "Властелин Колец",
+        "Властелин Колец",
+      ];
+      expect(result).toEqual(expected);
+    });
+    });
+
